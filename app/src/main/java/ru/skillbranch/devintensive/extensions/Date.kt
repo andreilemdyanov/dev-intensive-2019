8c74f52bdf2c01c94605d2a500c44a6f645ae763
andreilemdyanov <lemdyanov5@mail.ru>
2020-03-15 20:19:24 +0300

    TimeUnit plural and code refactoring.

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -29,36 +29,6 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 
 fun Date.humanizeDiff(date: Date = Date()): String {
 
-    val daysEndFirstPlus: MutableList<Int> = ArrayList()
-    val daysEndFirstMinus: MutableList<Int> = ArrayList()
-    val minutesEndFirstPlus: MutableList<Int> = ArrayList()
-    val minutesEndFirstMinus: MutableList<Int> = ArrayList()
-    val daysEndSecondPlus: MutableList<Int> = ArrayList()
-    val daysEndSecondMinus: MutableList<Int> = ArrayList()
-    val minutesEndSecondPlus: MutableList<Int> = ArrayList()
-    val minutesEndSecondMinus: MutableList<Int> = ArrayList()
-
-    for (i in 0..365) {
-        if (i % 10 in 2..4)
-            if (i !in 12..14 && i !in 112..114 && i !in 212..214 && i !in 312..314) {
-                daysEndFirstPlus.add(i)
-                daysEndFirstMinus.add(i.unaryMinus())
-                if (i < 50) {
-                    minutesEndFirstPlus.add(i)
-                    minutesEndFirstMinus.add(i.unaryMinus())
-                }
-            }
-        if (i % 10 == 1)
-            if (i != 11 && i != 111 && i != 211 && i != 311) {
-                daysEndSecondPlus.add(i)
-                daysEndSecondMinus.add(i.unaryMinus())
-                if (i < 50) {
-                    minutesEndSecondPlus.add(i)
-                    minutesEndSecondMinus.add(i.unaryMinus())
-                }
-            }
-    }
-
     var dif = this.time - date.time
     val positive = dif >= 0
     //Поправка
@@ -76,25 +46,13 @@ fun Date.humanizeDiff(date: Date = Date()): String {
         result =
             if (day < -360) "более года назад"
             else if ((day == -1L && hour < -2) || (-360 < day && day < -2))
-                when (day.toInt()) {
-                    in daysEndFirstMinus -> "$day дня назад"
-                    in daysEndSecondMinus -> "$day день назад"
-                    else -> "$day дней назад"
-                }
+                "${TimeUnits.DAY.plural(day.toInt().unaryMinus())} назад"
             else if (hour < -22 || (day == -1L && hour > -2)) "день назад"
             else if ((hour == -1L && min < -15) || (-22 < hour && hour < -2)) {
-                when (hour.toInt()) {
-                    in arrayOf(-2, -3, -4, -22) -> "$hour часа назад"
-                    in arrayOf(-1, -21) -> "$hour час назад"
-                    else -> "$hour часов назад"
-                }
+                "${TimeUnits.HOUR.plural(hour.toInt().unaryMinus())} назад"
             } else if (min < -45 || (hour == -1L && min > -15)) "час назад"
             else if ((min == -1L && sec < -15) || (-45 < min && min < -2)) {
-                when (min.toInt()) {
-                    in minutesEndFirstMinus -> "$min минуты назад"
-                    in minutesEndSecondMinus -> "$min минуту назад"
-                    else -> "$min минут назад"
-                }
+                "${TimeUnits.MINUTE.plural(min.toInt().unaryMinus())} назад"
             } else if (sec < -45 || (min == -1L && sec > -15)) "минуту назад"
             else if (-45 < sec && sec < -1) "несколько секунд назад"
             else "только что"
@@ -103,25 +61,13 @@ fun Date.humanizeDiff(date: Date = Date()): String {
         result =
             if (day > 360) "более чем через год"
             else if ((day == 1L && hour > 2) || day in 2..360)
-                when (day.toInt()) {
-                    in daysEndFirstPlus -> "через $day дня"
-                    in daysEndSecondPlus -> "через $day день"
-                    else -> "через $day дней"
-                }
+                "через ${TimeUnits.DAY.plural(day.toInt())}"
             else if (hour > 22 || (day == 1L && hour < 2)) "через день"
             else if ((hour == 1L && min > 15) || hour in 2..22) {
-                when (hour.toInt()) {
-                    in arrayOf(2, 3, 4, 22) -> "через $hour часа"
-                    in arrayOf(1, 21) -> "через $hour час"
-                    else -> "через $hour часов"
-                }
+                "через ${TimeUnits.HOUR.plural(hour.toInt())}"
             } else if (min > 45 || (hour == 1L && min < 15)) "через час"
             else if ((min == 1L && sec > 15) || min in 2..45) {
-                when (min.toInt()) {
-                    in minutesEndFirstPlus -> "через $min минуты"
-                    in minutesEndSecondPlus -> "через $min минуту"
-                    else -> "через $min минут"
-                }
+                "через ${TimeUnits.MINUTE.plural(min.toInt())}"
             } else if (sec > 45 || (min == 1L && sec < 15)) "через минуту"
             else if (sec in 1..45) "через несколько секунд"
             else "только что"
@@ -131,9 +77,55 @@ fun Date.humanizeDiff(date: Date = Date()): String {
     return result
 }
 
-enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+enum class TimeUnits : IUnitsCorrectOut {
+    SECOND {
+        override fun plural(value: Int): String {
+            return if (value % 10 in 2..4) {
+                if (value % 100 !in 12..14) "$value секунды"
+                else "$value секунд"
+            } else if (value % 10 == 1) {
+                if (value % 100 != 11) "$value секунду"
+                else "$value секунд"
+            } else "$value секунд"
+        }
+    },
+    MINUTE {
+        override fun plural(value: Int): String {
+            return if (value % 10 in 2..4) {
+                if (value % 100 !in 12..14) "$value минуты"
+                else "$value минут"
+            } else if (value % 10 == 1) {
+                if (value % 100 != 11) "$value минуту"
+                else "$value минут"
+            } else "$value минут"
+        }
+    },
+    HOUR {
+        override fun plural(value: Int): String {
+            return if (value % 10 in 2..4) {
+                if (value % 100 !in 12..14) "$value часа"
+                else "$value часов"
+            } else if (value % 10 == 1) {
+                if (value % 100 != 11) "$value час"
+                else "$value часов"
+            } else "$value часов"
+
+        }
+    },
+    DAY {
+        override fun plural(value: Int): String {
+            return if (value % 10 in 2..4) {
+                if (value % 100 !in 12..14)
+                    "$value дня"
+                else "$value дней"
+            } else if (value % 10 == 1) {
+                if (value % 100 != 11) "$value день"
+                else "$value дней"
+            } else "$value дней"
+        }
+    }
+}
+
+interface IUnitsCorrectOut {
+    fun plural(value: Int): String
 }
